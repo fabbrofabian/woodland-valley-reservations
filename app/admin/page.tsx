ae688e021976f7e8cdce3f880a2769093292631a
Woodland Valley <farm@woodlandvalley.com.au>
2026-06-27 21:51:11 +1000

    Add admin page

---
+++ app/admin/page.tsx
@@ -0,0 +1,8 @@
+export default function AdminPage() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Chicken Pickup Administration</h1>
+      <p>If you can see this, the /admin route is working.</p>
+    </main>
+  );
+}
